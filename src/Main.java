55e37c9ba37596ef74af41ce7d62e951da327941
David Achourbecove <kodziev@gmail.com>
2022-12-09 21:25:59 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+    class Solution {
+        public int removeElement(int[] nums, int val) {
+            int count =0;
+            for(int i=0;i<nums.length;i++){
+                if(nums[i] != val){
+                    nums[count] = nums[i];
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
